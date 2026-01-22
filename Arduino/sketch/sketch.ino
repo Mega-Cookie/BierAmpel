a18a9301366a08c26ab921f3a33a743d2fb1fbcb
Mega-Cookie <Lcs.Kp@protonmail.com>
2026-01-22 22:48:33 +0100

    Initial implimentation of new sensors

--- Arduino/sketch/sketch.ino
+++ Arduino/sketch/sketch.ino
@@ -1,15 +1,44 @@
-const int shakePin = 2; // Pin where sensor is connected
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <HX711.h>
+
+const int lightPin = 2;
+
+#define ONE_WIRE_BUS 3
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+const int weightPin =4;
+
+const int DOUT_PIN = 4;
+const int SCK_PIN = 5;
+HX711 scale;
+
 
 void setup() {
-  Serial.begin(9600);       // Initialize serial communication
-  pinMode(shakePin, INPUT); // Set the sensor pin as an input
+  Serial.begin(9600);
+  pinMode(lightPin, INPUT);
+  pinMode(weightPin, INPUT);
+  scale.begin(DOUT_PIN, SCK_PIN);
+  scale.set_scale();
+  scale.tare();
+  sensors.begin();
 }
 
 void loop() {
-  int shakeState = digitalRead(shakePin); // Read digital signal (0 or 1)
+  int lightState = digitalRead(lightPin); 
+  sensors.requestTemperatures();
+  float tempCelsius = sensors.getTempCByIndex(0);
+  if (scale.is_ready()) {
+    Serial.print(scale.get_units(), 1);
+    Serial.print(",");
+  } else {
+    Serial.print("HX711 nicht gefunden.");
+    Serial.print(",");
+  }
+  Serial.print(lightState);
+  Serial.print(",");
+  Serial.println(tempCelsius);
   
-  // Plotter expects only numerical values and a newline character
-  Serial.println(shakeState); 
-  
-  delay(10); // Small delay to stabilize the plot
+  delay(500);
 }
