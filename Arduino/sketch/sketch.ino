cbca743be88d7f3285bd4a2f8e3ea7b95f20b30e
Mega-Cookie <lcs.kp@protonmail.com>
2026-01-24 20:11:16 +0100

    add GPIO Serial output

--- Arduino/sketch/sketch.ino
+++ Arduino/sketch/sketch.ino
@@ -1,6 +1,7 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <HX711.h>
+#include <SoftwareSerial.h>
 
 unsigned long lastTime = 0;
 const unsigned long interval = 1000;
@@ -18,8 +19,11 @@ const int DOUT_PIN = 4;
 const int SCK_PIN = 5;
 HX711 scale;
 
+SoftwareSerial piSerial(10, 11);
+
 void setup() {
   Serial.begin(9600);
+  piSerial.begin(9600);
   pinMode(lightPin, INPUT);
 //  pinMode(weightPin, INPUT);
   pinMode(buzzPin, OUTPUT);
@@ -30,22 +34,17 @@ void setup() {
   sensors.begin();
 }
 
+void multiPrint(String message) {
+  Serial.print(message);   // Print to USB
+  piSerial.print(message); // Print to GPIO
+}
+
 void loop() {
   unsigned long currentTime = millis();
   if (currentTime - lastTime >= interval) {
     lastTime = currentTime;
-    int lightState = !digitalRead(lightPin); 
-    sensors.requestTemperatures();
-    float tempCelsius = sensors.getTempCByIndex(0);
-    if (scale.is_ready()) {
-      Serial.print("Gewicht:");
-      Serial.print(scale.get_units(), 1);
-      Serial.print(",");
-    } else {
-      Serial.print("HX711 nicht gefunden.");
-      Serial.print(",");
-    }
-    if (lightState == 1) {
+    int lightState = !digitalRead(lightPin);
+        if (lightState == 1) {
       trigger++;
     } else {
       trigger=0;
@@ -57,13 +56,17 @@ void loop() {
       alarm = 0;
       digitalWrite(buzzPin, LOW);
     }
-    Serial.print("Licht:");
-    Serial.print(lightState);
-    Serial.print(",");
-    Serial.print("Alarm:");
-    Serial.print(alarm);
-    Serial.print(",");
-    Serial.print("Temperatur:");
-    Serial.println(tempCelsius);
+
+    sensors.requestTemperatures();
+    float tempCelsius = sensors.getTempCByIndex(0);
+
+    if (scale.is_ready()) {
+      multiPrint("Gewicht:" + String(scale.get_units(), 1) + ",");
+    } else {
+      multiPrint("HX711 nicht gefunden.,");
+    }
+    multiPrint("Licht:" + String(lightState) + ",");
+    multiPrint("Alarm:" + String(alarm) + ",");
+    multiPrint("Temperatur:" + String(tempCelsius) + "\n");
   }  
 }
